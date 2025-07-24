706311cf3819684b035990113369ad0273146a8e
Iryna Abrosimova <abrosimovairyna@gmail.com>
2025-07-24 14:55:23 +0200

    add day 1

---
+++ Day 1/day-1.py
@@ -0,0 +1,40 @@
+# print("Hello World")
+
+# print("Hello World\nHello World")
+
+# print('Hello' + ' ' + "Irena")
+
+# input("What is your name?")
+
+# print("Hello " + input("What is your name?"))
+
+# print("Hello " + input("What is your name?") + '!')
+
+# name = "Jack"
+# print(name)
+# name = "Angela"
+# print(name)
+
+# name = input("What is your name?")
+# print(name)
+
+# name = input("What is your name?")
+# name_length = len(name)
+# print("Your name has " + str(name_length) + " characters")
+
+# # We have 2 variables glass1 and glass2. glass1 contains milk and glass2 contains juice.
+# # Write 3 lines of code to switch the contents of the variables.
+# # You are not allowed to type the words "milk" or "juice".
+# # You are only allowed to use variables to solve this exercise.
+# glass1 = "milk"
+# glass2 = "juice"
+# glass1 = glass1 + glass2
+# glass2 = glass1[:4]
+# glass1 = glass1[4:]
+# print(glass1)
+# print(glass2)
+
+print("Welcome to the Band Name Generator.")
+city_name = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+print("Your band name could be: " + city_name + " " + pet_name)
